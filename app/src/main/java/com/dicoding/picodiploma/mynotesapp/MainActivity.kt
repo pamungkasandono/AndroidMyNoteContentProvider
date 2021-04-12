18ed457d8dcff1501995b3597b79f966ffcee1c9
pamungkas andono <pamungkas.org@gmail.com>
2021-04-12 14:44:06 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/picodiploma/mynotesapp/MainActivity.kt
@@ -0,0 +1,155 @@
+package com.dicoding.picodiploma.mynotesapp
+
+import android.content.Intent
+import android.database.ContentObserver
+import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.dicoding.picodiploma.mynotesapp.adapter.NoteAdapter
+import com.dicoding.picodiploma.mynotesapp.databinding.ActivityMainBinding
+import com.dicoding.picodiploma.mynotesapp.db.DatabaseContract.NoteColumns.Companion.CONTENT_URI
+import com.dicoding.picodiploma.mynotesapp.db.NoteHelper
+import com.dicoding.picodiploma.mynotesapp.entity.Note
+import com.dicoding.picodiploma.mynotesapp.helper.MappingHelper
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var adapter: NoteAdapter
+
+    private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        private const val EXTRA_STATE = "EXTRA_STATE"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.title = "Notes"
+
+        binding.rvNotes.layoutManager = LinearLayoutManager(this)
+        binding.rvNotes.setHasFixedSize(true)
+        adapter = NoteAdapter(this)
+        binding.rvNotes.adapter = adapter
+
+        binding.fabAdd.setOnClickListener {
+            val intent = Intent(this@MainActivity, NoteAddUpdateActivity::class.java)
+//            startActivityForResult(intent, NoteAddUpdateActivity.REQUEST_ADD)
+            startActivity(intent)
+        }
+
+        val handlerThread = HandlerThread("DataObserver")
+        handlerThread.start()
+        val handler = Handler(handlerThread.looper)
+
+        val myObserver = object : ContentObserver(handler) {
+            override fun onChange(selfChange: Boolean) {
+                loadNotesAsync()
+            }
+        }
+
+        contentResolver.registerContentObserver(CONTENT_URI, true, myObserver)
+
+        if (savedInstanceState == null) {
+            loadNotesAsync()
+        } else {
+            val list = savedInstanceState.getParcelableArrayList<Note>(EXTRA_STATE)
+            if (list != null) {
+                adapter.listNotes = list
+            }
+        }
+    }
+
+    private fun loadNotesAsync() {
+        GlobalScope.launch(Dispatchers.Main) {
+            binding.progressbar.visibility = View.VISIBLE
+            val noteHelper = NoteHelper.getInstance(applicationContext)
+            noteHelper.open()
+            val deferredNotes = async(Dispatchers.IO) {
+                // CONTENT_URI = content://com.dicoding.picodiploma.mynotesapp/note
+//                val cursor = noteHelper.queryAll()
+                val cursor = contentResolver.query(CONTENT_URI, null, null, null, null)
+                MappingHelper.mapCursorToArrayList(cursor)
+            }
+            binding.progressbar.visibility = View.INVISIBLE
+            val notes = deferredNotes.await()
+            noteHelper.close()
+            if (notes.size > 0) {
+                adapter.listNotes = notes
+            } else {
+                adapter.listNotes = ArrayList()
+                showSnackbarMessage("Tidak ada data saat ini")
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelableArrayList(EXTRA_STATE, adapter.listNotes)
+    }
+
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//
+//        if (data != null) {
+//            when (requestCode) {
+//                // Akan dipanggil jika request codenya ADD
+//                NoteAddUpdateActivity.REQUEST_ADD -> if (resultCode == NoteAddUpdateActivity.RESULT_ADD) {
+//                    val note = data.getParcelableExtra<Note>(NoteAddUpdateActivity.EXTRA_NOTE) as Note
+//
+//                    adapter.addItem(note)
+//                    binding.rvNotes.smoothScrollToPosition(adapter.itemCount - 1)
+//
+//                    showSnackbarMessage("Satu item berhasil ditambahkan")
+//                }
+//                // Update dan Delete memiliki request code sama akan tetapi result codenya berbeda
+//                NoteAddUpdateActivity.REQUEST_UPDATE ->
+//                    when (resultCode) {
+//                        /*
+//                        Akan dipanggil jika result codenya  UPDATE
+//                        Semua data di load kembali dari awal
+//                        */
+//                        NoteAddUpdateActivity.RESULT_UPDATE -> {
+//
+//                            val note = data.getParcelableExtra<Note>(NoteAddUpdateActivity.EXTRA_NOTE) as Note
+//                            val position = data.getIntExtra(NoteAddUpdateActivity.EXTRA_POSITION, 0)
+//
+//                            adapter.updateItem(position, note)
+//                            binding.rvNotes.smoothScrollToPosition(position)
+//
+//                            showSnackbarMessage("Satu item berhasil diubah")
+//                        }
+//                        /*
+//                        Akan dipanggil jika result codenya DELETE
+//                        Delete akan menghapus data dari list berdasarkan dari position
+//                        */
+//                        NoteAddUpdateActivity.RESULT_DELETE -> {
+//                            val position = data.getIntExtra(NoteAddUpdateActivity.EXTRA_POSITION, 0)
+//
+//                            adapter.removeItem(position)
+//
+//                            showSnackbarMessage("Satu item berhasil dihapus")
+//                        }
+//                    }
+//            }
+//        }
+//    }
+
+    /**
+     * Tampilkan snackbar
+     *
+     * @param message inputan message
+     */
+    private fun showSnackbarMessage(message: String) {
+        Snackbar.make(binding.rvNotes, message, Snackbar.LENGTH_SHORT).show()
+    }
+}
---
+++ app/src/main/java/com/dicoding/picodiploma/mynotesapp/provider/NoteProvider.kt
@@ -0,0 +1,80 @@
+package com.dicoding.picodiploma.mynotesapp.provider
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.content.Context
+import android.content.UriMatcher
+import android.database.Cursor
+import android.net.Uri
+import android.util.Log
+import com.dicoding.picodiploma.mynotesapp.db.DatabaseContract.NoteColumns.Companion.AUTHORITY
+import com.dicoding.picodiploma.mynotesapp.db.DatabaseContract.NoteColumns.Companion.CONTENT_URI
+import com.dicoding.picodiploma.mynotesapp.db.DatabaseContract.NoteColumns.Companion.TABLE_NAME
+import com.dicoding.picodiploma.mynotesapp.db.NoteHelper
+
+class NoteProvider : ContentProvider() {
+
+    companion object {
+        private const val NOTE = 1
+        private const val NOTE_ID = 2
+        private val sUriMatcher = UriMatcher(UriMatcher.NO_MATCH)
+        private lateinit var noteHelper: NoteHelper
+
+        init {
+            // content://com.dicoding.picodiploma.mynotesapp/note
+            sUriMatcher.addURI(AUTHORITY, TABLE_NAME, NOTE)
+
+            // content://com.dicoding.picodiploma.mynotesapp/note/id
+            sUriMatcher.addURI(AUTHORITY, "$TABLE_NAME/#", NOTE_ID)
+        }
+    }
+
+    override fun onCreate(): Boolean {
+        noteHelper = NoteHelper.getInstance(context as Context)
+        noteHelper.open()
+        return true
+    }
+
+    override fun query(uri: Uri, projection: Array<String>?, selection: String?,
+                       selectionArgs: Array<String>?, sortOrder: String?): Cursor? {
+        Log.d("asdasd", uri.pathSegments.size.toString())
+//        sUriMatcher.match(uri) -->  menghasilkan jumlah array sama saja dengan uri.pathSegments.size
+        return when (sUriMatcher.match(uri)) {
+            NOTE -> noteHelper.queryAll()
+            NOTE_ID -> noteHelper.queryById(uri.lastPathSegment.toString())
+            else -> null
+        }
+    }
+
+    override fun getType(uri: Uri): String? {
+        return null
+    }
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        val added: Long = when (NOTE) {
+            sUriMatcher.match(uri) -> noteHelper.insert(values)
+            else -> 0
+        }
+        context?.contentResolver?.notifyChange(CONTENT_URI, null)
+        return Uri.parse("$CONTENT_URI/$added")
+    }
+
+    override fun update(uri: Uri, values: ContentValues?, selection: String?,
+                        selectionArgs: Array<String>?): Int {
+        val update: Int = when (NOTE_ID) {
+            sUriMatcher.match(uri) -> noteHelper.update(uri.lastPathSegment.toString(), values)
+            else -> 0
+        }
+        context?.contentResolver?.notifyChange(CONTENT_URI, null)
+        return update
+    }
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
+        val deleted: Int = when (NOTE_ID) {
+            sUriMatcher.match(uri) -> noteHelper.deleteById(uri.lastPathSegment.toString())
+            else -> 0
+        }
+        context?.contentResolver?.notifyChange(CONTENT_URI, null)
+        return deleted
+    }
+}
